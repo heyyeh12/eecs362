e2bf45a9e9e7fc32993ace40afbe0e68134c8f01
Allisonnorah <anhurst@comcast.net>
2016-01-19 23:35:55 -0600

    Create xor_gate.v

---
+++ alu/xor_gate.v
@@ -0,0 +1,19 @@
+/*
+XOR GATE
+*/
+
+
+module or_gate (a, b, z)
+
+// Ports
+
+input a, b;
+output z;
+
+// Implementation
+
+always @(a or b)
+    begin
+        assign z = a ^ b;
+    end
+endmodule
