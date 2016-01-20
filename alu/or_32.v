18907cd9c70a7aa39cf154b65c64201d838c5f0b
Allisonnorah <anhurst@comcast.net>
2016-01-20 01:24:27 -0600

    Create or_32.v
    UNTESTED

---
+++ alu/or_32.v
@@ -0,0 +1,21 @@
+
+// 32-bit OR GATE
+
+// Questions: are always statements allowed/useful/necessary?
+
+
+module or_32 (a, b, z)
+
+// Ports
+
+    input [31:0] a, b;
+    output [31:0] z;
+
+// Implementation
+    genvar i;
+    generate
+        for(i=0; i<32; i=i+1)
+            or_gate bit_i (a[i], b[i], z[i]);
+        end
+    endgenerate
+endmodule
